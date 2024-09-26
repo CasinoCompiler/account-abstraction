8eaaccebd2d2807a632ea5446e7e24e70149970d
CC <casinocompiler@gmail.com>
2024-09-26 17:09:41 +0100

    init

---
+++ src/zksync/ZKSMinimalAccount.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+/**
+ * @title
+ * @author 
+ * @notice 
+ * @dev 
+ */
+
+/** Imports */
+// @Order Imports, Interfaces, Libraries, Contracts
+
+contract ZKSMinimalAccount {
+
+    /** Errors */   
+
+    /** Type Declarations */
+
+    /** State Variables */
+
+    /** Events */
+
+    /** Constructor */
+
+    /** Modifiers */
+
+    /** Functions */
+    // @Order recieve, fallback, external, public, internal, private
+    receive() external payable{}
+    fallback() external payable{}
+
+
+    /** Getter Functions */
+
+}
